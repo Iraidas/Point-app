2569bf064e248a3f907013ae16dd4f2df063a3e1
iraida <ira.serq.1996@gmail.com>
2023-03-09 13:23:00 +0300

    добавлено меню для мобильной версии

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+// Управление видео
+
 var video = document.querySelector(".preview__video");
 var muteBtn = document.querySelector(".preview__video-mute-button");
 
@@ -15,6 +17,22 @@ video.addEventListener('click', function() {
     else 
         video.pause();
 })
+
+
+
+// Бургер меню
+
+let burgerBtn = document.querySelector(".header__burger-menu")
+let burgerNav = document.querySelector(".header__nav")
+
+burgerBtn.addEventListener('click', function () {
+  burgerBtn.classList.toggle("header__burger-menu-active");
+  burgerNav.classList.toggle("header__nav-active");
+})
+
+
+
+// Аккордион для вопросов
 
 let accor = document.querySelectorAll(".questions__accordion");
 
